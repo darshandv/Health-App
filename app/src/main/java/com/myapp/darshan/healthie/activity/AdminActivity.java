5d275601702a41ba8051b74f21a675392f51b3e2
16CO216 <darshandv10@gmail.com>
2018-06-05 10:02:02 +0530

    Database working with admin

--- app/src/main/java/com/myapp/darshan/healthie/activity/AdminActivity.java
+++ app/src/main/java/com/myapp/darshan/healthie/activity/AdminActivity.java
@@ -2,27 +2,35 @@ package com.myapp.darshan.healthie.activity;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.myapp.darshan.healthie.R;
 
 public class AdminActivity extends AppCompatActivity {
 
     public EditText tip_layout ;
     private String tip;
+    private String tip_data;
     private DatabaseReference mFirebaseDatabase;
     private FirebaseDatabase mFirebaseInstance;
     private Button btn_sign_out;
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authListener;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +39,10 @@ public class AdminActivity extends AppCompatActivity {
 
         tip_layout = (EditText)findViewById(R.id.input_tip);
         btn_sign_out = (Button)findViewById(R.id.sign_out);
-        tip = tip_layout.getText().toString();
         auth = FirebaseAuth.getInstance();
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Tips");
 
         authListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -49,6 +58,45 @@ public class AdminActivity extends AppCompatActivity {
 
             }
         };
+
+        FloatingActionButton fab =
+                (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                tip = tip_layout.getText().toString();
+
+                if (!tip.trim().equals(""))
+                {
+                    FirebaseDatabase.getInstance().getReference().child("Tips")
+                            .setValue(tip);
+                }
+
+                // Clear the input
+                tip_layout.setText("");
+            }
+        });
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                tip_data = dataSnapshot.getValue(String.class);
+
+                Toast.makeText(getApplicationContext(),"You received : "+tip_data,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Failure", "Failed to read value.", error.toException());
+            }
+        });
+
+
 
         btn_sign_out.setOnClickListener(new View.OnClickListener() {
             @Override
